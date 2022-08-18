d5d0e7a3e6875996da2419d1a675d89b02db3809
Watcahrapol <watcharapol.tree@kmutt.ac.th>
2022-08-18 16:30:07 +0700

    up

--- bonus_1.cpp
+++ bonus_1.cpp
@@ -10,7 +10,7 @@ int main()
 {
     ios_base::sync_with_stdio();cin.tie(0);
     cout << fixed << setprecision(2);
-    int Mybirth = 6, last_id = 1, last2_id = 2, table[12][12] = {};     //Birthday : Friday ,  ID : 64090500421
+    int Mybirth = 6, last_id = 1, last2_id = 2, tabel[12][12] = {};     //Birthday : Friday ,  ID : 64090500421
     struct info tree, hero, monster;
     
     srand (time(NULL));
@@ -18,17 +18,17 @@ int main()
     hero.x = Mybirth; hero.y = last_id; hero.num = 2;
     monster.x = 10 - Mybirth; monster.y = last2_id; monster.num = 3;
     
-    table[tree.y - 1][tree.x - 1] = tree.num;
-    table[hero.y - 1][hero.x - 1] = hero.num;
-    table[monster.y - 1][monster.x - 1] = monster.num;
+    tabel[tree.y - 1][tree.x - 1] = tree.num;
+    tabel[hero.y - 1][hero.x - 1] = hero.num;
+    tabel[monster.y - 1][monster.x - 1] = monster.num;
 
     cout << "\tInfo\nBirth day : Friday (6)\nID : 64090500421\nTree : " << tree.x << "," << tree.y << " \nHero : " << hero.x << "," << hero.y << "\nMonster : " << monster.x << "," << monster.y;
-    cout << "\n   *** table! *** \n";
+    cout << "\n   *** tabel! *** \n";
     for(int i = 0; i < 10; i++)
     {
         for(int j = 0; j < 10; j++)
         {
-            cout << table[i][j] << " ";
+            cout << tabel[i][j] << " ";
         }
         cout << endl;
     }
