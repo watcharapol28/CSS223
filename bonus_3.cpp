c6da2af6518c18b5c8ab4f21252a3785218ff105
Watcahrapol <watcharapol.tree@kmutt.ac.th>
2022-09-05 23:38:38 +0700

    up

---
+++ bonus_3.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        string name;
+        int score;
+        Node* next;
+        Node* prev;
+};
+
+class 
+
+
+int main()
+{
+
+}
